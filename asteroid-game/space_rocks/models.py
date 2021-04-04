d73beb6fce9391db508c8cb4eda3f1f7884c06c3
Daniel Hangan <hangandaniel1@gmail.com>
2021-04-04 22:35:26 +0300

    project finished

--- asteroid-game/space_rocks/models.py
+++ asteroid-game/space_rocks/models.py
@@ -1,7 +1,7 @@
 from pygame.math import Vector2
 from pygame.transform import rotozoom
 
-from utils import load_sprite, wrap_position
+from utils import load_sprite, load_sound, wrap_position, get_random_velocity
 
 UP = Vector2(0, -1)
 
@@ -16,9 +16,6 @@ class GameObject:
         blit_position = self.position - Vector2(self.radius)
         surface.blit(self.sprite, blit_position)
 
-    def move(self):
-        self.position = self.position + self.velocity
-
     def collides_with(self, other_obj):
         distance = self.position.distance_to(other_obj.position)
         return distance < self.radius + other_obj.radius
@@ -29,8 +26,11 @@ class GameObject:
 class Spaceship(GameObject):
     MANEUVERABILITY = 3
     ACCELERATION = 0.25
+    BULLET_SPEED = 3
 
-    def __init__(self, position):
+    def __init__(self, position, create_bullet_callback):
+        self.create_bullet_callback = create_bullet_callback
+        self.laser_sound = load_sound("laser")
         # Make a copy of the original UP vector
         self.direction = Vector2(UP)
 
@@ -50,3 +50,43 @@ class Spaceship(GameObject):
 
     def accelerate(self):
         self.velocity += self.direction * self.ACCELERATION
+
+    def shoot(self):
+        bullet_velocity = self.direction * self.BULLET_SPEED + self.velocity
+        bullet = Bullet(self.position, bullet_velocity)
+        self.create_bullet_callback(bullet)
+        self.laser_sound.play()
+
+class Asteroid(GameObject):
+    def __init__(self, position, create_asteroid_callback,size=3):
+        self.create_asteroid_callback = create_asteroid_callback
+        self.size = size
+
+        size_to_scale = {
+            3: 1,
+            2: 0.5,
+            1: 0.25
+        }
+
+        scale = size_to_scale[size]
+        sprite = rotozoom(load_sprite("asteroid"), 0, scale)
+
+
+        super().__init__(
+                position, sprite, get_random_velocity(1,3)
+            )
+
+    def split(self):
+        if self.size > 1:
+            for _ in range(2):
+                asteroid = Asteroid(
+                    self.position, self.create_asteroid_callback, self.size - 1
+                )
+                self.create_asteroid_callback(asteroid)
+
+class Bullet(GameObject):
+    def __init__(self, position, velocity):
+        super().__init__(position, load_sprite("bullet"), velocity)
+    
+    def move(self, surface):
+        self.position = self.position + self.velocity
